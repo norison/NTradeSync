8b3aa1b9d8e410e036b073aece6150196832ab76
Ihor Matiev <48079411+norison@users.noreply.github.com>
2024-07-11 20:02:36 +0300

    Added user secrets and options for c-trader (#6)

---
+++ src/NTradeSync.Api/Extensions/WebApplicationBuilderExtensions.cs
@@ -0,0 +1,24 @@
+﻿using NTradeSync.Infrastructure.Options;
+
+namespace NTradeSync.Api.Extensions;
+
+public static class WebApplicationBuilderExtensions
+{
+    public static WebApplicationBuilder AddApi(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddControllers();
+        
+        return builder;
+    }
+    
+    public static WebApplicationBuilder AddInfrastructure(this WebApplicationBuilder builder)
+    {
+        builder.Services
+            .AddOptions<CTraderOptions>()
+            .BindConfiguration("Terminals:CTrader")
+            .ValidateDataAnnotations()
+            .ValidateOnStart();
+        
+        return builder;
+    }
+}
---
+++ src/NTradeSync.Infrastructure/Options/CTraderOptions.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NTradeSync.Infrastructure.Options;
+
+public class CTraderOptions
+{
+    [Required] public string ClientId { get; set; } = string.Empty;
+    [Required] public string ClientSecret { get; set; } = string.Empty;
+}
